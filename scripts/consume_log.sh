f8ddae01afd13408e256a8d0098d5ea4f6c54daf
Martin Minkov <minkovlmartin@gmail.com>
2024-07-12 17:51:13 -0700

    refactor(consume_log.sh): change URL construction to include offset in the path instead of query parameters for cleaner API design feat(consume_log.sh): add verbose output and debug logging to curl request for better visibility and debugging feat(produce_log.sh): add verbose output and debug logging to curl request for better visibility and debugging docs(consume_log.sh, produce_log.sh): add comments to explain the purpose and usage of the scripts for improved maintainability

--- scripts/consume_log.sh
+++ scripts/consume_log.sh
@@ -1,18 +1,20 @@
-
 #!/bin/bash
 
 # Define the URL
-URL=${1:-"http://localhost:8080"}
+BASE_URL=${1:-"http://localhost:8080"}
 
 # Define the offset
 OFFSET=${2:-0}
 
-# Construct the JSON payload
-DATA="{\"offset\": $OFFSET}"
+# Construct the full URL with the offset in the path
+URL="${BASE_URL}/${OFFSET}"
 
-# Make the curl GET request
-# Note: We use -G to append data to the URL for a GET request
-curl -X GET "$URL" -G --data-urlencode "$(echo $DATA)"
+# Make the curl GET request with verbose output
+curl -X GET "$URL" \
+     -H "Content-Type: application/json" \
+     --max-time 5 \
+     -v \
+     2>&1 | sed 's/^/[DEBUG] /'
 
 # Add a newline for better formatting of output
 echo
--- scripts/produce_log.sh
+++ scripts/produce_log.sh
@@ -7,8 +7,11 @@ VALUE=${2:-"TGV0J3MgR28gIzEK"}
 # Construct the JSON payload
 DATA="{\"record\": {\"value\": \"$VALUE\"}}"
 
-# Make the curl POST request
-curl -X POST $URL -d "$DATA"
+# Make the curl POST request with verbose output
+curl -X POST $URL -d "$DATA" \
+     -H "Content-Type: application/json" \
+     -v \
+     2>&1 | sed 's/^/[DEBUG] /'
 
 # Add a newline for better formatting of output
 echo
