33e244721073a8a813b46240e8175c80c8c84a8a
Martin Minkov <minkovlmartin@gmail.com>
2024-07-20 13:07:00 -0700

    feat(log): add segment struct and constructor function to manage log segments
    A new segment struct is introduced to represent a log segment, which consists of an index and a store. The segment struct holds the base offset, next offset, and configuration for the segment.
    
    The newSegment constructor function is added to create a new segment. It takes the directory, base offset, and configuration as parameters. The function opens the store and index files, creates the store and index instances, and initializes the next offset based on the last index entry. If the index is empty, the next offset is set to the base offset.
    
    This change allows the log package to manage log segments, which are essential for storing and accessing log data efficiently. The segment struct encapsulates the index and store, providing a higher-level abstraction for working with log segments.

---
+++ internal/log/segment.go
@@ -0,0 +1,47 @@
+package log
+
+import (
+	"fmt"
+	"os"
+	"path"
+)
+
+type segment struct {
+	index                  *index
+	store                  *store
+	baseOffset, nextOffset uint64
+	config                 Config
+}
+
+func newSegment(dir string, baseOffset uint64, c Config) (*segment, error) {
+	s := &segment{
+		baseOffset: baseOffset,
+		config:     c,
+	}
+	var err error
+
+	storeFile, err := os.OpenFile(path.Join(dir, fmt.Sprintf("%d%s", baseOffset, ".store")), os.O_RDWR|os.O_CREATE|os.O_APPEND, 0644)
+	if err != nil {
+		return nil, err
+	}
+	if s.store, err = newStore(storeFile); err != nil {
+		return nil, err
+	}
+
+	indexFile, err := os.OpenFile(path.Join(dir, fmt.Sprintf("%d%s", baseOffset, ".index")), os.O_RDWR|os.O_CREATE, 0644)
+	if err != nil {
+		return nil, err
+	}
+	if s.index, err = newIndex(indexFile, c); err != nil {
+		return nil, err
+	}
+
+	if off, _, err := s.index.Read(-1); err != nil {
+		// If the index is empty, we set the next offset to the initial offset.
+		s.nextOffset = baseOffset
+	} else {
+		// Otherwise, we set the next offset to the offset of the last index entry.
+		s.nextOffset = baseOffset + uint64(off) + 1
+	}
+	return s, nil
+}
