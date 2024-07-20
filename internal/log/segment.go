97f010296568ac576d83be8ff677f2a5805204b4
Martin Minkov <minkovlmartin@gmail.com>
2024-07-20 13:36:53 -0700

    feat(segment): implement Append, Read, Remove, Close and IsMaxed methods
    - Append method writes the record to the store and index
    - Read method retrieves the record from the store using the index
    - Remove method removes the segment by closing the index and store and deleting the index file
    - Close method closes the index and store
    - IsMaxed method checks if the segment has reached its max size based on the configured max store and index bytes
    - Add helper function nearestMultiple to calculate the nearest multiple of two numbers
    
    refactor(segment): add comments to explain baseOffset and nextOffset fields
    
    - baseOffset is used to calculate the relative offset of the index since there can be multiple segments
    - nextOffset keeps track of the next offset to be used when appending a record

--- internal/log/segment.go
+++ internal/log/segment.go
@@ -4,13 +4,17 @@ import (
 	"fmt"
 	"os"
 	"path"
+
+	api "github.com/MartinMinkov/proglog/api/v1"
+	"google.golang.org/protobuf/proto"
 )
 
 type segment struct {
-	index                  *index
-	store                  *store
-	baseOffset, nextOffset uint64
-	config                 Config
+	index      *index
+	store      *store
+	baseOffset uint64 // The base offset is used to calculate the relative offset of the index. Because we can have multiple segments, we need to keep track of the base offset for each segment.
+	nextOffset uint64 // The next offset to be used when appending a record
+	config     Config
 }
 
 func newSegment(dir string, baseOffset uint64, c Config) (*segment, error) {
@@ -44,4 +48,71 @@ func newSegment(dir string, baseOffset uint64, c Config) (*segment, error) {
 		s.nextOffset = baseOffset + uint64(off) + 1
 	}
 	return s, nil
+}
+
+func (s *segment) Append(record *api.Record) (offset uint64, err error) {
+	// The record offset is set to the next offset
+	curr := s.nextOffset
+	record.Offset = curr
+
+	p, err := proto.Marshal(record)
+	if err != nil {
+		return 0, err
+	}
+	_, pos, err := s.store.Append(p)
+	if err != nil {
+		return 0, err
+	}
+	// Index offsets are relative to the base offset of the segment
+	offset = s.nextOffset - uint64(s.baseOffset)
+	if err = s.index.Write(uint32(offset), pos); err != nil {
+		return 0, err
+	}
+	// Increment the next offset
+	s.nextOffset++
+	return curr, nil
+}
+
+func (s *segment) Read(off uint64) (*api.Record, error) {
+	// We need to convert the absolute offset to a relative offset that we can use for the index.
+	_, pos, err := s.index.Read(int64(off - s.baseOffset))
+	if err != nil {
+		return nil, err
+	}
+	p, err := s.store.Read(pos)
+	if err != nil {
+		return nil, err
+	}
+	record := &api.Record{}
+	err = proto.Unmarshal(p, record)
+	return record, err
+}
+
+func (s *segment) Remove() error {
+	if err := s.index.Close(); err != nil {
+		return err
+	}
+	if err := s.store.Close(); err != nil {
+		return err
+	}
+	return os.Remove(s.index.Name())
+}
+
+func (s *segment) Close() error {
+	if err := s.index.Close(); err != nil {
+		return err
+	}
+	if err := s.store.Close(); err != nil {
+		return err
+	}
+	return nil
+}
+
+func (s *segment) IsMaxed() bool {
+	return s.store.size >= s.config.Segment.MaxStoreBytes || s.index.size >= s.config.Segment.MaxIndexBytes
+}
+
+func nearestMultiple(j, k uint64) uint64 {
+	return (j / k) * k
+
 }
